9fbdcd59c53637377d4d0a195b115f032628ef1e
Casper Hsiao <powen.hsiao@duke.edu>
2022-11-28 19:27:03 -0500

    Modify CustomerScreen UI.

--- ui/src/views/CustomerScreen.vue
+++ ui/src/views/CustomerScreen.vue
@@ -5,48 +5,46 @@
     <h2>Orders</h2>
     <b-button @click="refresh" class="mb-2">Refresh</b-button>
     <b-table v-if="customer" :items="customer.orders" :fields="orderFields"/>
-    
-    <h2>Draft Order</h2>
-    <p>Add the ingredients you want:</p>
+    <h2>Shop</h2>
     <b-button-group>
       <b-button
-        v-for="(ing, idx) in possibleIngredients.keys()"
+        v-for="(prod, idx) in inventory"
         :key="idx"
         variant="primary"
-        @click="addIngredient(ing)"
+        @click="addToCart(prod.name)"
       >
-        Add {{ ing }}
+        Add {{ prod.name }}
       </b-button>
     </b-button-group>
-    <h3>
-      Ingredients
-    </h3>
+    <h3>Your Cart</h3>
     <b-list-group flush>
       <b-list-group-item
-        v-for="(ing, idx) in draftOrderIngredients"
+        v-for="(ing, idx) in cart"
         :key="idx"
         class="d-flex justify-content-between align-items-center"
       >
         <span title="ing">{{ ing }}</span>
-        <b-button variant="danger" @click="deleteIngredient(idx)">Delete</b-button>
+        <b-button variant="danger" @click="removeFromCart(idx)">Delete</b-button>
       </b-list-group-item>
     </b-list-group>
     <h3>
       Total cost: {{ totalCost }}
     </h3>
-    <div class="mt-2">
-      <b-button @click="save">Save</b-button>
+    <b-row align-h="start">
+    <div class="mt-2 mx-1">
+      <b-button @click="saveCart">Save Cart</b-button>
     </div>
-    <div class="mt-2">
-      <b-button @click="submit">Submit</b-button>
-      Note: must save before submitting
+    <div class="mt-2 mx-1">
+      <b-button @click="checkout">Checkout</b-button>
+      Note: must save cart before checking out
     </div>
+  </b-row>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref, computed, Ref } from 'vue'
-import { CustomerWithOrders, Ingredient } from "../../../server/data"
+import { CustomerWithOrders, Product } from "../../../server/data"
 
 // props
 interface Props {
@@ -61,57 +59,57 @@ const props = withDefaults(defineProps<Props>(), {
 const customer: Ref<CustomerWithOrders | null> = ref(null)
 
 const name = computed(() => customer.value?.name || props.customerId)
-const draftOrderIngredients: Ref<string[]> = ref([])
-const possibleIngredients: Ref<Map<string, number>> = ref(new Map<string, number>)
+const cart: Ref<string[]> = ref([])
+const inventory: Ref<Product[]> = ref([])
 const totalCost = computed(() => {
-  return draftOrderIngredients.value.reduce((previousValue, currentValue) => {
-    const currCost = possibleIngredients.value.get(currentValue)
-    if (currCost) {
-      return previousValue + currCost
+  return cart.value.reduce((previousValue, currentValue) => {
+    const prod = inventory.value.find(prod => {
+      return prod.name === currentValue
+    })
+    if (prod) {
+      return previousValue + prod.price
     }
     return previousValue}, 0)
 })
 
 async function refresh() {
-  (await (await fetch("/api/possible-ingredients")).json()).forEach((element: Ingredient) => {
-    possibleIngredients.value.set(element.name, element.price)
-  });
+  inventory.value = await (await fetch("/api/inventory")).json()
 
   if (props.customerId) {
     customer.value = await (await fetch("/api/customer/" + encodeURIComponent(props.customerId))).json()
-    draftOrderIngredients.value = (await (await fetch("/api/customer/" + encodeURIComponent(props.customerId) + "/draft-order")).json())?.ingredientIds || []
+    cart.value = (await (await fetch("/api/customer/" + encodeURIComponent(props.customerId) + "/cart")).json())?.productIds || []
   }
 }
 onMounted(refresh)
 
-const orderFields = [{key: '_id', label: 'Order ID  '}, 'state', {key: 'ingredientIds', label: 'Ingredients', formatter: (value: string[]) => {return value.join(", ")}}]
+const orderFields = [{key: '_id', label: 'Order ID'}, 'state', {key: 'productIds', label: 'Products', formatter: (value: string[]) => {return value.join(", ")}}]
 
-async function save() {
+async function saveCart() {
   await fetch(
-    "/api/customer/" + encodeURIComponent(props.customerId) + "/draft-order",
+    "/api/customer/" + encodeURIComponent(props.customerId) + "/update-cart",
     {
       headers: {
         "Content-Type": "application/json",
       },
       method: "PUT",
-      body: JSON.stringify({ ingredientIds: draftOrderIngredients.value })
+      body: JSON.stringify({ productIds: cart.value })
     }
   )
 }
 
-async function submit() {
+async function checkout() {
   await fetch(
-    "/api/customer/" + encodeURIComponent(props.customerId) + "/submit-draft-order",
+    "/api/customer/" + encodeURIComponent(props.customerId) + "/checkout-cart",
     { method: "POST" }
   )
   await refresh()
 }
 
-async function addIngredient(ingredient: string) {
-  draftOrderIngredients.value.push(ingredient) 
+async function addToCart(product: string) {
+  cart.value.push(product) 
 }
 
-async function deleteIngredient(idx: number) {
-  draftOrderIngredients.value.splice(idx, 1)
+async function removeFromCart(idx: number) {
+  cart.value.splice(idx, 1)
 }
 </script>
