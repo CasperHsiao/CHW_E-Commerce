50e9b61de7d18a8d338459e9a169337866841c02
Casper Hsiao <powen.hsiao@duke.edu>
2022-11-29 20:41:23 -0500

    Rename and modify app home screen.

---
+++ ui/src/views/HomeScreen.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mx-3 my-3">
+    <b-jumbotron header="Welcome CHW Shopping Center!" />
+    <h3 v-if="user.name">Navigate to My Orders to start shopping</h3>
+    <h3 v-if="!user.name">Please login to start shopping</h3>
+  </div>
+</template>
+
+<script setup lang="ts">import { BJumbotron } from 'bootstrap-vue';
+import { Ref, inject } from 'vue';
+
+const user: Ref<any> = inject("user")!
+</script>
