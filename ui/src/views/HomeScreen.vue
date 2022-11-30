66ddd58a4f3132ef3704153059d4a0bff78efaf3
Casper Hsiao <powen.hsiao@duke.edu>
2022-11-29 21:33:08 -0500

    Modify home page UI to accomodate both customer and operator.

--- ui/src/views/HomeScreen.vue
+++ ui/src/views/HomeScreen.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="mx-3 my-3">
     <b-jumbotron header="Welcome CHW Shopping Center!" />
-    <h3 v-if="user.name">Navigate to My Orders to start shopping</h3>
-    <h3 v-if="!user.name">Please login to start shopping</h3>
+    <h3 v-if="user?.roles?.includes('customer')">Navigate to My Orders to start shopping</h3>
+    <h3 v-if="user?.roles?.includes('operator')">Navigate to My Work Screen to start operating</h3>
+    <h3 v-if="!user.name">Please login to navigate</h3>
   </div>
 </template>
 
